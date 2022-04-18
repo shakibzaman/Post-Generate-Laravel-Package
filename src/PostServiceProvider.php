e04c33968b5594cbc37dad703adb14f120e5a2ff
shakibzaman <zaman.shakib@gmail.com>
2022-04-18 11:06:53 +0600

    #Updated

--- src/PostServiceProvider.php
+++ src/PostServiceProvider.php
@@ -5,20 +5,17 @@ class PostServiceProvider extends \Illuminate\Support\ServiceProvider{
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/views','Post');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->SeederResources();
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views','Post');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->publishes([
+            __DIR__.'/../database/seeds' => database_path('seeds'),
+        ], 'post-seeds');
 
     }
     public function register()
     {
 
     }
-    protected function SeederResources()
-    {
-        $this->publishes([
-            __DIR__ . '/database/seeds/PostSeeder.php' => database_path('seeds/PostSeeder.php'),
-        ]);
-    }
+
 }
